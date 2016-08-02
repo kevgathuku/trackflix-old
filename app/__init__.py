b51357a58f6e4309ef9e1618811c0a69ece24652
Kevin Ndung'u <kevgathuku@gmail.com>
2016-08-02 17:09:27 +0300

    Initialize Flask SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+# Load configuration defaults from the config.py file in the root
+# This file will be imported and the config extracted from it
+# http://flask.pocoo.org/docs/0.11/api/#flask.Config.from_object
+app.config.from_object('config')
+
+# Initialize SQLAlchemy db
+db = SQLAlchemy(app)
+
+# Import the views handling the routes
 from app import views

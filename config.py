effb723806eb93b5b99239ccec4367186019bcf3
Kevin Ndung'u <kevgathuku@gmail.com>
2016-08-03 12:20:47 +0300

    Turn off the Flask-SQLAlchemy event system
    Remove the Flask SQLAlchemy warning

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
+
+# Turn off the Flask-SQLAlchemy event system
+SQLALCHEMY_TRACK_MODIFICATIONS = False

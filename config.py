38bd289b90e931a79918b8a805497ea13dcb06b3
Kevin Ndung'u <kevgathuku@gmail.com>
2016-08-03 15:29:59 +0300

    Handle cases where the DATABASE_URL is an env var
    This adds compatibility with systems such as Heroku

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+if os.environ.get('DATABASE_URL'):
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
 
 # Turn off the Flask-SQLAlchemy event system

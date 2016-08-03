92f1f42261dac5ab691ae8f525816727cc5d17c8
Kevin Ndung'u <kevgathuku@gmail.com>
2016-08-03 18:37:11 +0300

    Enable loading env config from .env file

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 # All configs should be uppercase
 import os
 
+from dotenv import load_dotenv
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path)
 
 if os.environ.get('DATABASE_URL'):
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 else:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 

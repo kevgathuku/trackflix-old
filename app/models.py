385dc5902885d034d1ef214089e2af36e4a09952
Kevin Ndung'u <kevgathuku@gmail.com>
2016-08-03 11:44:52 +0300

    Complete designing DB models

--- app/models.py
+++ app/models.py
@@ -3,6 +3,19 @@ from datetime import datetime
 from app import db
 
 
+show_people = db.Table(
+    'show_people',
+    db.Column('person_id', db.Integer, db.ForeignKey('person.id')),
+    db.Column('show_id', db.Integer, db.ForeignKey('show.id')),
+    db.Column('role', db.String(100))
+)
+
+show_genres = db.Table(
+    'show_genres',
+    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id')),
+    db.Column('show_id', db.Integer, db.ForeignKey('show.id'))
+)
+
 class Show(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True)
@@ -13,29 +26,109 @@ class Show(db.Model):
     backdrop_path = db.Column(db.String(100))
     first_air_date = db.Column(db.Date)
     origin_country = db.Column(db.String(10))
+    popularity = db.Column(db.Float)
+
+    seasons = db.relationship('Season', backref='show', lazy='dynamic')
+
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    # What to refer the model as in the corresponding Model
-    creators = db.relationship(
-        'Person', backref='created_shows', lazy='dynamic')
-    external_ids = db.relationship(
-        'ExternalId', backref='show', lazy='dynamic')
+    def __repr__(self):
+        return '<Show: %r>' % self.name
 
 
-class ExternalId(db.Model):
+class ExternalService(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    service = db.Column(db.String(20))
+    name = db.Column(db.String(20), unique=True)
+    prefix_url = db.Column(db.String(120))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Service: %r>' % self.name
+
+class PersonExternalId(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.Integer, db.ForeignKey('externalservice.id'))
     identifier = db.Column(db.String(150), unique=True)
 
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class ShowExternalId(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.Integer, db.ForeignKey('externalservice.id'))
+    identifier = db.Column(db.String(150))
+
     show_id = db.Column(db.Integer, db.ForeignKey('show.id'))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    biography = db.Column(db.Date)
-    birthday = db.Column(db.Text)
+    biography = db.Column(db.Text)
+    birthday = db.Column(db.Date)
 
-    shows = db.Column(db.Integer, db.ForeignKey('show.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Person: %r>' % self.name
+
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    # Genres have a TVDB id
+    service = db.Column(db.Integer, db.ForeignKey('externalservice.id'))
+    identifier = db.Column(db.String(150))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Genre: %r>' % self.name
+
+
+class Network(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    # Networks have a TVDB id
+    service = db.Column(db.Integer, db.ForeignKey('externalservice.id'))
+    identifier = db.Column(db.String(150))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Network: %r>' % self.name
+
+
+class Season(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.SmallInteger)
+    poster_path = db.Column(db.String(100))
+    air_date = db.Column(db.Date)
+    episode_count = db.Column(db.SmallInteger)
+    show_id = db.Column(db.Integer, db.ForeignKey('show.id'))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Season: %r>' % self.number
